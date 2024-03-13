294e13ef85bf7be47bad92c58b4881c441524c51
GrishaShatLav <120121078+GrishaShatLav@users.noreply.github.com>
2024-03-13 17:19:33 +0300

    Test_1

---
+++ Test_1/Test1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Circumference");
+            double Circum = Convert.ToDouble(Console.ReadLine());
+            double Radius = (double)Circum/(2*Math.PI);
+            double Square = (double)Math.PI*Math.Pow(Radius, 2);
+            Console.WriteLine("Radius = {0}", Radius);
+            Console.WriteLine("Square = {0}", Square);
+            Console.ReadKey();
+
+
+        }
+    }
+}
