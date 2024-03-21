e734f1447b53b84b522c31b04d01e57ef7c0de68
GrishaShatLav <120121078+GrishaShatLav@users.noreply.github.com>
2024-03-21 23:12:43 +0300

    Test_9_Lesson_10

---
+++ Test_9_Lesson_10/Test_9_Lesson_10/Angle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_9_Lesson_10
+{
+    internal class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+
+            set
+
+            {
+                if (value < 0 || value > 360)
+                    Console.WriteLine("Gradus Error!");
+                else
+                    gradus = value;
+            }
+
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+
+            set
+
+            {
+                if (value < 1 || value > 60)
+                    Console.WriteLine("Min Error!");
+                else
+                    min = value;
+            }
+
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+
+            set
+
+            {
+                if (value < 0 || value > 60)
+                    Console.WriteLine("Sec Error!");
+                else
+                    sec = value;
+            }
+
+        }
+
+
+        public Angle(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+
+        public double ToRadians()
+        {
+            return (gradus  + (min  + sec/60)/60)*Math.PI/180;
+        }
+    }
+}
+
+
---
+++ Test_9_Lesson_10/Test_9_Lesson_10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+//  Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут, sec - угловых секунд.
+//  Реализовать класс, в котором указанные значения представлены в виде свойств. Для свойств предусмотреть проверку корректности данных.
+//  Класс должен содержать конструктор для установки начальных значений, а также метод ToRadians для перевода угла в радианы.
+//  Создать объект на основе разработанного класса.
+//  Осуществить использование объекта в программе.
+
+namespace Test_9_Lesson_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Gradus, Min and Sec");
+            int gradus_ = Convert.ToInt32(Console.ReadLine());
+            int min_ = Convert.ToInt32(Console.ReadLine());
+            int sec_ = Convert.ToInt32(Console.ReadLine());
+
+            Angle angle = new Angle(gradus_, min_, sec_);
+
+        
+
+
+            double Radians = angle.ToRadians();
+            Console.WriteLine(Radians);
+            Console.ReadKey();
+
+
+        }
+
+    }
+
+
+}
