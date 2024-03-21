c322b63808346e28391acd15a0457340a6a5c108
GrishaShatLav <120121078+GrishaShatLav@users.noreply.github.com>
2024-03-22 02:01:01 +0300

    Test_11_Lesson_12

---
+++ Test_11_Lesson_12/Test_11_Lesson_12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Reflection.Metadata;
+using System.Drawing;
+
+
+//Разработать статический класс для работы с окружностью. Класс должен содержать 3 метода:
+
+//метод, определяющий длину окружности по заданному радиусу;
+//метод, определяющий площадь круга по заданному радиусу;
+//метод, проверяющий принадлежность точки с координатами (x, y) кругу с радиусом r и координатами центра x0, y0.
+
+
+namespace Test_11_Lesson_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plese, enter radius");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Plese, enter x and y");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double length = Circle.GetLength(r);
+            double square = Circle.GetSquare(r);
+            string check = Circle.GetCheck(r, x, y);
+         
+            Console.WriteLine($"{length} \n{square} \n{check}");
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
---
+++ Test_11_Lesson_12/Test_11_Lesson_12/Sphere.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_11_Lesson_12
+{
+    static class Circle
+    {
+        static public double GetLength(double r)
+        {
+            return Math.PI*2*r;
+        }
+        static public double GetSquare(double r)
+        {
+            return Math.PI * r * r;
+        }
+        static public string GetCheck(double r, double x, double y)
+        {
+            double hypotenuse = Math.Sqrt(x * x + y * y);
+            if (hypotenuse <= r) { return "Точка принадлежит кругу"; }
+            else
+            { return "Точка НЕ принадлежит кругу"; }
+        }
+    }
+}
