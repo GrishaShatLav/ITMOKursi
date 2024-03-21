e9bea54d29be17508c14748eb12ce4269ac29f3f
GrishaShatLav <120121078+GrishaShatLav@users.noreply.github.com>
2024-03-22 01:19:03 +0300

    Test_10_Lesson_11

---
+++ Test_10_Lesson_11/Test_10_Lesson_11/Lineage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_10_Lesson_11
+{
+    struct Lineage
+    {
+        double k;
+        double b;
+        
+
+        public Lineage(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+            
+        }
+
+        public string Root()
+        {
+           
+            if (k==0)
+            {
+                return "Решений нет";
+            }
+            else if (k==0 && b==0)
+            {
+                return "x - любое число";
+            }
+            double x = -b /k ;
+           
+            return $"x = {x}";
+        }
+    }
+}
---
+++ Test_10_Lesson_11/Test_10_Lesson_11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Reflection.Metadata;
+
+//Разработать структуру для решения линейного уравнения 0=kx+b.
+//Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа.
+//Для решения уравнения предусмотреть метод Root. Создать экземпляр разработанной структуры.
+//Осуществить использование экземпляра в программе.
+namespace Test_10_Lesson_11
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+
+            Lineage lineage = new Lineage(k, b);
+            Console.WriteLine(lineage.Root());
+            Console.ReadKey();
+        }
+
+
+    }
+
+
+
+
+}
