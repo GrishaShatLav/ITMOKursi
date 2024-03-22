fdbd6135099192e2c5870b4d7f58eed2d8ea1789
GrishaShatLav <120121078+GrishaShatLav@users.noreply.github.com>
2024-03-22 23:38:39 +0300

    Test_16_Lesson_17

---
+++ Test_16_Lesson_17/Test_16_Lesson_17/Bank.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Test_16_Lesson_17
+{
+    internal class Bank<T>
+    {
+        private T Num { get; set; }
+        private int Balance { get; set; }
+        private string Fio { get; set; }
+
+        public string GetInfo()
+        {
+            return $"{Num} {Balance} {Fio}";
+
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Enter number");
+            Num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Enter Balance");
+            Balance = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            Fio = (Console.ReadLine());
+        }
+    }
+}
+
+    
---
+++ Test_16_Lesson_17/Test_16_Lesson_17/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Reflection.Metadata;
+using System.Drawing;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json.Serialization;
+using System.Xml.Linq;
+
+/*Создать класс для моделирования счета в банке. 
+ * Предусмотреть закрытые поля для номера счета, баланса, ФИО владельца.  
+ * Класс должен быть объявлен как обобщенный. Универсальный параметр T должен определять тип номера счета. 
+ * Разработать  методы  для  доступа  к  данным  –  заполнения  и  чтения. Создать  несколько экземпляров класса, параметризированного различными типам. 
+ * Заполнить его поля и вывести на экран информацию об экземпляре класса.*/
+
+
+namespace Test_16_Lesson_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank<int> bank = new Bank<int>();
+            bank.Input();
+            Console.WriteLine(bank.GetInfo());
+            Bank<string> bank2 = new Bank<string>();
+            bank2.Input();
+            Console.WriteLine(bank2.GetInfo());      
+            Bank<double> bank3 = new Bank<double>();
+            bank3.Input();
+            Console.WriteLine(bank3.GetInfo());
+
+            Console.ReadKey();
+
+        }
+
+
+    }
+
+
+}
